d66cc7cc80fe2ce95bdf3916dcc26a8e1d8d2802
Soseeska <astoyan99@mail.ru>
2022-08-22 17:41:41 +0300

    Created ProductList and Controller

---
+++ eCommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dataContext.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
