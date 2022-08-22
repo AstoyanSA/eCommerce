36f0c9e7f8da26851d9b03ff261886425442885b
Soseeska <astoyan99@mail.ru>
2022-08-22 20:16:31 +0300

    Added Categories

--- eCommerce/Server/Migrations/20220822171438_Categories.Designer.cs
+++ eCommerce/Server/Migrations/20220822171438_Categories.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerce.Server.Data;
 
@@ -10,9 +11,10 @@ using eCommerce.Server.Data;
 namespace eCommerce.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220822171438_Categories")]
+    partial class Categories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
