5cb3e758228eeb16734c46e4940c34cfaa38143d
Soseeska <astoyan99@mail.ru>
2022-08-22 18:23:11 +0300

    Added ProductService To Client

--- eCommerce/Client/Program.cs
+++ eCommerce/Client/Program.cs
@@ -1,3 +1,6 @@
+global using eCommerce.Shared;
+global using System.Net.Http.Json;
+global using eCommerce.Client.Services.ProductService;
 using eCommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
