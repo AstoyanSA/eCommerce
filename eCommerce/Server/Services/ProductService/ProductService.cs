05063d8dbf73e43097e46d770087c11e18b9a8f8
Soseeska <astoyan99@mail.ru>
2022-08-22 19:11:03 +0300

    Added GetProduct Function

--- eCommerce/Server/Services/ProductService/ProductService.cs
+++ eCommerce/Server/Services/ProductService/ProductService.cs
@@ -8,6 +8,24 @@
         {
             _dataContext = dataContext;
         }
+
+        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<Product>();
+            var product = await _dataContext.Products.FindAsync(productId);
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Sorry, but this product does not exist.";
+            }
+            else
+            {
+                response.Data = product;
+            }
+
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
         {
             var response = new ServiceResponse<List<Product>>
